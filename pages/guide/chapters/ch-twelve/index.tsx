3e18084c4c91042ef256f2f3e8331279d411924d
michal tomek <95964850+rampepurda@users.noreply.github.com>
2023-04-24 12:53:37 +0200

    maintenance

--- pages/guide/chapters/ch-twelve/index.tsx
+++ pages/guide/chapters/ch-twelve/index.tsx
@@ -36,34 +36,35 @@ const ChTwelve: NextPage = () => {
 
           <ul className='hasTypeDecimal'>
             <li>
-              <h5>Feature overview - Installation</h5>
+              <h4>Feature overview - Installation</h4>
               <ul className='hasTypeDisc'>
                 <li>
-                  <h5>Apollo GraphQL - server</h5>
+                  <h5 style={{color: '#FFA500FF'}}>Apollo GraphQL - server</h5>
                   git clone https://github.com/apollographql/odyssey-lift-off-part1
                 </li>
                 <li>
-                  <h5>Apollo Client</h5>
+                  <h5 style={{color: '#FFA500FF'}}>Apollo Client</h5>
                   yarn add @apollo/client
                 </li>
               </ul>
             </li>
-            <li>Feature data requirements</li>
             <li>
-              Schema definition language (SDL)
+              <h4>Feature data requirements</h4>
+            </li>
+            <li>
+              <h4>Schema definition language (SDL)</h4>
               <h5>A schema is like a contract between the server and the client. It defines what a GraphQL API can and can't do, and how clients can request or change data. It's an abstraction layer that provides flexibility to consumers while hiding backend implementation details.</h5>
               <hr />
               <h5>Definition Language, or SDL.</h5>
               <AlertBox className={'info'}>
-                type SpaceCat &#123;<br/>
-                &nbsp;&nbsp;age: Int<br/>
-                &nbsp;&nbsp;missions: [Mission]<br/>
-                &#125;
+                <ul>
+                  <li>type SpaceCat &#123;</li>
+                  <li>&nbsp;&nbsp;age: Int! // can`t be 'null'</li>
+                  <li>&nbsp;&nbsp;missions: [Mission]</li>
+                  <li>&#125;</li>
+                </ul>
               </AlertBox>
             </li>
-            <li></li>
-            <li></li>
-            <li></li>
           </ul>
         </div>
       </div>
