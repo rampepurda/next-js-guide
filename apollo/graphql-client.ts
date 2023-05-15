c9619c54d3e52b084ae20ecbb5d6af3d97deeb9a
michal tomek <95964850+rampepurda@users.noreply.github.com>
2023-05-15 11:14:01 +0200

    NG-10 | GrpahQL_Install Local Server apollo: graphql-client.ts | maintenance

--- apollo/graphql-client.ts
+++ apollo/graphql-client.ts
@@ -6,7 +6,7 @@ import { environment  } from '../configuration/environment'
 
 const apolloClient = new ApolloClient({
   uri: environment.countriesURL,
-  cache: new InMemoryCache(),
-});
+  cache: new InMemoryCache()
+})
 
 export default apolloClient
