d4971dadbb806a30219583fb9a4c81a7296b36d8
michal tomek <95964850+rampepurda@users.noreply.github.com>
2023-06-21 17:31:56 +0200

    next.config.js - add Webpack

--- next.config.js
+++ next.config.js
@@ -14,7 +14,22 @@ const nextConfig = {
         pathname: '/*/*',
       },
     ],
-  }
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgo: false,
+          },
+        },
+      ],
+    })
+
+    return config
+  },
 }
 
 
