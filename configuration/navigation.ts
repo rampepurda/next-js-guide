c2137b8eca099f9ef9265bf02c874fb143d489f0
michal tomek <95964850+rampepurda@users.noreply.github.com>
2023-07-19 09:26:17 +0200

    NG-27 | Ch18 Next 13 All About (#26)
    * NG-27 | Ch18 Next 13.4.7 all about
    pages: Add Ch18 Next13
    
    * pages:ch18 add page/ structure

--- configuration/navigation.ts
+++ configuration/navigation.ts
@@ -1,98 +1,102 @@
-import { ROUTE } from "./routes"
+import { ROUTE } from './routes'
 
 export const navigationLinks = [
   {
-    tKey: "navMain.guide",
-    link: ROUTE.GUIDE
+    tKey: 'navMain.guide',
+    link: ROUTE.GUIDE,
   },
   {
-    tKey: "navMain.projects",
-    link: ROUTE.PROJECTS
-  }
+    tKey: 'navMain.projects',
+    link: ROUTE.PROJECTS,
+  },
 ]
 
 export const navigationGuideLinks = [
   {
-    tKey: "navGuide.ch-one",
-    link: `${ROUTE.GUIDE_CHAPTERS}/ch-one`
+    tKey: 'navGuide.ch-one',
+    link: `${ROUTE.GUIDE_CHAPTERS}/ch-one`,
   },
   {
-    tKey: "navGuide.ch-two",
-    link: `${ROUTE.GUIDE_CHAPTERS}/ch-two`
+    tKey: 'navGuide.ch-two',
+    link: `${ROUTE.GUIDE_CHAPTERS}/ch-two`,
   },
   {
-    tKey: "navGuide.ch-three",
-    link: `${ROUTE.GUIDE_CHAPTERS}/ch-three`
+    tKey: 'navGuide.ch-three',
+    link: `${ROUTE.GUIDE_CHAPTERS}/ch-three`,
   },
   {
-    tKey: "navGuide.ch-four",
-    link: `${ROUTE.GUIDE_CHAPTERS}/ch-four`
+    tKey: 'navGuide.ch-four',
+    link: `${ROUTE.GUIDE_CHAPTERS}/ch-four`,
   },
   {
-    tKey: "navGuide.ch-five",
-    link: `${ROUTE.GUIDE_CHAPTERS}/ch-five`
+    tKey: 'navGuide.ch-five',
+    link: `${ROUTE.GUIDE_CHAPTERS}/ch-five`,
   },
   {
-    tKey: "navGuide.ch-six",
-    link: `${ROUTE.GUIDE_CHAPTERS}/ch-six`
+    tKey: 'navGuide.ch-six',
+    link: `${ROUTE.GUIDE_CHAPTERS}/ch-six`,
   },
   {
-    tKey: "navGuide.ch-seven",
-    link: `${ROUTE.GUIDE_CHAPTERS}/ch-seven`
+    tKey: 'navGuide.ch-seven',
+    link: `${ROUTE.GUIDE_CHAPTERS}/ch-seven`,
   },
   {
-    tKey: "navGuide.ch-eight",
-    link: `${ROUTE.GUIDE_CHAPTERS}/ch-eight`
+    tKey: 'navGuide.ch-eight',
+    link: `${ROUTE.GUIDE_CHAPTERS}/ch-eight`,
   },
   {
-    tKey: "navGuide.ch-nine",
-    link: `${ROUTE.GUIDE_CHAPTERS}/ch-nine`
+    tKey: 'navGuide.ch-nine',
+    link: `${ROUTE.GUIDE_CHAPTERS}/ch-nine`,
   },
   {
-    tKey: "navGuide.ch-ten",
-    link: `${ROUTE.GUIDE_CHAPTERS}/ch-ten`
+    tKey: 'navGuide.ch-ten',
+    link: `${ROUTE.GUIDE_CHAPTERS}/ch-ten`,
   },
   {
-    tKey: "navGuide.ch-eleven",
-    link: `${ROUTE.GUIDE_CHAPTERS}/ch-eleven`
+    tKey: 'navGuide.ch-eleven',
+    link: `${ROUTE.GUIDE_CHAPTERS}/ch-eleven`,
   },
   {
-    tKey: "navGuide.ch-twelve",
-    link: `${ROUTE.GUIDE_CHAPTERS}/ch-twelve`
+    tKey: 'navGuide.ch-twelve',
+    link: `${ROUTE.GUIDE_CHAPTERS}/ch-twelve`,
   },
   {
-    tKey: "navGuide.ch-thirteen",
-    link: `${ROUTE.GUIDE_CHAPTERS}/ch-thirteen`
+    tKey: 'navGuide.ch-thirteen',
+    link: `${ROUTE.GUIDE_CHAPTERS}/ch-thirteen`,
   },
   {
-    tKey: "navGuide.ch-fourteen",
-    link: `${ROUTE.GUIDE_CHAPTERS}/ch-fourteen`
+    tKey: 'navGuide.ch-fourteen',
+    link: `${ROUTE.GUIDE_CHAPTERS}/ch-fourteen`,
   },
   {
-    tKey: "navGuide.ch-fifteen",
-    link: `${ROUTE.GUIDE_CHAPTERS}/ch-fifteen`
+    tKey: 'navGuide.ch-fifteen',
+    link: `${ROUTE.GUIDE_CHAPTERS}/ch-fifteen`,
   },
   {
-    tKey: "navGuide.ch-sixteen",
-    link: `${ROUTE.GUIDE_CHAPTERS}/ch-sixteen`
+    tKey: 'navGuide.ch-sixteen',
+    link: `${ROUTE.GUIDE_CHAPTERS}/ch-sixteen`,
   },
   {
-    tKey: "navGuide.ch-seventeen",
-    link: `${ROUTE.GUIDE_CHAPTERS}/ch-seventeen`
+    tKey: 'navGuide.ch-seventeen',
+    link: `${ROUTE.GUIDE_CHAPTERS}/ch-seventeen`,
+  },
+  {
+    tKey: 'navGuide.ch-eighteen',
+    link: `${ROUTE.GUIDE_CHAPTERS}/ch-eighteen`,
   },
 ]
 
 export const navigationProjectsLinks = [
   {
-    tKey: "navProjectsLeft.dynamicRouter",
-    link: ROUTE.PROJECT_DYN_ROUTE
+    tKey: 'navProjectsLeft.dynamicRouter',
+    link: ROUTE.PROJECT_DYN_ROUTE,
   },
   {
-    tKey: "navProjectsLeft.cars",
-    link: ROUTE.PROJECT_CARS
+    tKey: 'navProjectsLeft.cars',
+    link: ROUTE.PROJECT_CARS,
   },
   {
-    tKey: "navProjectsLeft.countries",
-    link: ROUTE.PROJECT_COUNTRIES
+    tKey: 'navProjectsLeft.countries',
+    link: ROUTE.PROJECT_COUNTRIES,
   },
 ]
