6e7bf5674b1aed2601522b72e46b6f318414b7f8
michal tomek <95964850+rampepurda@users.noreply.github.com>
2023-09-01 16:45:27 +0200

    NG-37 | maintenance

--- slices/Countries/countrySelectors.ts
+++ slices/Countries/countrySelectors.ts
@@ -1,17 +1,27 @@
-import { createSelector } from "@reduxjs/toolkit"
-import { Country } from "../../types"
+import { createSelector } from '@reduxjs/toolkit'
+import { Country } from '../../types'
+import { AppState } from '../../store'
+
+const selectCountries = (state: AppState) => state.Countries.countries
+const selectCountrySearch = (state: AppState) => state.Countries.searchCountriesValue
 
 /**
  * Try to define reusable selectors alongside their corresponding reducers.
  * input selectors
  */
 
-const selectCountries = (state: any) => state.Countries.countries
-const selectCountrySearch = (state: any) => state.Countries.searchCountriesValue
-
 export const selectCountryFilter = createSelector(
   selectCountries,
   selectCountrySearch,
-  (countries, searchCountriesValue ) => {
-    return countries.filter((country: Country) => country.name.toLowerCase().substring(0,3) === searchCountriesValue || country.name.toLowerCase() === searchCountriesValue)
-  })
+  (countries, searchCountriesValue) => {
+    return countries.filter(
+      (country: Country) =>
+        /**
+         * The substring() method of String values returns the part of this string from the start index up to and excluding the end index, or to the end of the string if no end index is supplied.
+         * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/substring
+         */
+        country.name.toLowerCase().substring(0, 3) === searchCountriesValue ||
+        country.name.toLowerCase() === searchCountriesValue
+    )
+  }
+)
