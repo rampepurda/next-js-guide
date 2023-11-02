cdfb61cc2ce43bbc406385b0e831c81a38f5d134
michal tomek <95964850+rampepurda@users.noreply.github.com>
2023-11-02 16:34:15 +0100

    guide/ch25 description is finished

--- pages/guide/chapters/ch-twentyFive/index.tsx
+++ pages/guide/chapters/ch-twentyFive/index.tsx
@@ -4,6 +4,8 @@ import { breakPoints, navigationGuideLinks } from '../../../../configuration'
 import Head from 'next/head'
 import { useWindWidth } from '../../../../hooks'
 import { useEffect } from 'react'
+import { merriweather, roboto } from '../../../../fonts'
+import Link from 'next/link'
 
 const ChTwentyFive: NextPage = () => {
   const { windowSize, getWindWidth } = useWindWidth()
@@ -26,7 +28,47 @@ const ChTwentyFive: NextPage = () => {
         )}
 
         <div className="col-9">
-          <h2>25. Font - how to work in Next and how to setup</h2>
+          <h2>25. Font Module - how to work in Next and how to setup</h2>
+          <Link href="https://nextjs.org/docs/app/api-reference/components/font" target="_blank">
+            read more about Font Module
+          </Link>
+          <hr />
+
+          <InfoBox className={'isInfo'}>
+            <h4>
+              This API reference will help you understand how to use{' '}
+              <Link
+                href="https://nextjs.org/docs/app/building-your-application/optimizing/fonts#google-fonts"
+                target="_blank"
+              >
+                next/font/google
+              </Link>
+              &nbsp;and&nbsp;
+              <Link
+                href="https://nextjs.org/docs/app/building-your-application/optimizing/fonts#local-fonts"
+                target="_blank"
+              >
+                next/font/local
+              </Link>
+              . For features and usage, please see the Optimizing Fonts page.
+            </h4>
+          </InfoBox>
+
+          <h2>Google Font example</h2>
+          <ul className="hasTypeDisc">
+            <li>create Component: Font</li>
+            <li>Import Comp Font in to the other Component</li>
+          </ul>
+          <h3 className={roboto.className}>Font - Roboto</h3>
+          <code>
+            &lt;h3 className=&#123;<strong>roboto.className</strong>&#125;&gt; Font - Roboto
+            &lt;/h3&gt;
+          </code>
+          <h3 className={merriweather.className}>Font - Merriweather</h3>
+          <code>
+            &lt;h3 className=&#123;<strong>merriweather.className</strong>&#125;&gt; Font -
+            Merriweather &lt;/h3&gt;
+          </code>
         </div>
       </div>
     </>
