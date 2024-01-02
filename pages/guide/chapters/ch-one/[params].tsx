c771fd8c526b903f1c7bdbad9fa77b9d6f5de7f5
michal tomek <95964850+rampepurda@users.noreply.github.com>
2024-01-02 20:43:23 +0100

    main | maintenace

--- pages/guide/chapters/ch-one/[params].tsx
+++ pages/guide/chapters/ch-one/[params].tsx
@@ -1,31 +1,17 @@
 import { useRouter } from 'next/router'
-import { Countries, InfoBox } from '../../../../components'
+import { InfoBox } from '../../../../components'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
-import { useAppSelector } from '../../../../store'
-
 const ChOneSlug: () => void = () => {
-  const { ch1SlugNavigation } = useAppSelector((state) => state.Common)
-  const { countries } = useAppSelector((state) => state.Countries)
   const path = usePathname()
   const router = useRouter()
   const { params = [] } = router.query
-  //const pathName = Array.from(params)
-  const linksArray = Array.from(ch1SlugNavigation)
-  const navLink: string = 'Countries'
-  const nameArray: string[] = ['Countries', 'leonor', 'darmojed']
-  const activeLink: string = '/guide/chapters/ch-one'
-  const mockData: string[] = ['Audi', 'BMW', 'Fiat', 'Hyundai', 'Kia']
+  const pathName = Array.from(params)
 
   return (
     <>
       <Link href="/guide/chapters/ch-one">Go back</Link>
-      <hr />
-
-      {ch1SlugNavigation.filter((i) => `${activeLink}/${i}` === path) && (
-        <Countries countries={countries.slice(1, 5)} />
-      )}
       <hr />
 
       {params.length >= 1 && (
