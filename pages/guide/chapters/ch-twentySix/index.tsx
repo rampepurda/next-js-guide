ebf10efdf08260e42bb65451ebb5ba3df92ee8f1
michal tomek <95964850+rampepurda@users.noreply.github.com>
2024-01-04 14:08:04 +0100

    NG-48 | useForm in Next

--- pages/guide/chapters/ch-twentySix/index.tsx
+++ pages/guide/chapters/ch-twentySix/index.tsx
@@ -4,11 +4,40 @@ import { breakPoints, navigationGuideLinks } from '../../../../configuration'
 import Head from 'next/head'
 import { useWindWidth } from '../../../../hooks'
 import { useEffect } from 'react'
-import { merriweather, roboto } from '../../../../fonts'
 import Link from 'next/link'
 
 const ChTwentySix: NextPage = () => {
   const { windowSize, getWindWidth } = useWindWidth()
+  /*
+            const submitUpdatedProfile = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const form = event.currentTarget
+    const formData = new FormData(event.currentTarget)
+    const formDataObject = Object.fromEntries(formData)
+    const response = await fetch('', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application.json' },
+      body: JSON.stringify(formDataObject),
+    })
+
+    try {
+      if (response) {
+        // WILL BE REMOVED-ONLY TESTING PURPOSE!!
+        alert(
+          `First Name: ${formDataObject.firstName}
+           Last Name: ${formDataObject.lastName}
+            Date Of Birth: ${formDataObject.dateOfBirth}
+            CountrySelected: ${formDataObject.countrySelected}
+            ZipCode: ${formDataObject?.zipCountryCode}
+           Status: SUCCESSFULLY SEND`
+        )
+        reset()
+      }
+    } catch (err) {
+      alert(`${err}`)
+    }
+  }
+   */
 
   useEffect(() => {
     getWindWidth()
@@ -48,59 +77,69 @@ const ChTwentySix: NextPage = () => {
           </ul>
           <hr />
 
+          <h3>Installation:</h3>
+          <ul className="hasTypeDisc hasVerticalPadding-3">
+            <li>npm install react-hook-form</li>
+            <li>yarn react-hook-form</li>
+          </ul>
+
+          <hr />
+
+          <h3>Usage:</h3>
+
           <code>
-            import &#123; useForm, type FieldValues &#125; from &apos;react-hook-form&apos;
+            import &#123; useForm, <mark>type FieldValues</mark> &#125; from
+            &apos;react-hook-form&apos;
           </code>
 
           <h3>const register = &#123;</h3>
           <ul className="">
             <li>
               <mark>register</mark>
+              <h4>
+                This method allows you to register an input or select element and apply validation
+                rules to React Hook Form. Validation rules are all based on the HTML standard and
+                also allow for custom validation methods.
+              </h4>
             </li>
             <li>
               <mark>handleSubmit</mark>
+              <h4>This function will receive the form data if form validation is successful.</h4>
             </li>
             <li>
               <mark>formState: &#123; errors, isSubmitting &#125;</mark>
+              <h4>
+                This object contains information about the entire form state. It helps you to keep
+                on track with the user's interaction with your form application.
+              </h4>
             </li>
             <li>
               <mark>getValues</mark>
+              <h4>
+                An optimized helper for reading form values. The difference between watch and
+                getValues is that getValues will not trigger re-renders or subscribe to input
+                changes.
+              </h4>
             </li>
             <li>
               <mark>reset</mark>
+              <h4>
+                Reset the entire form state, fields reference, and subscriptions. There are optional
+                arguments and will allow partial form state reset.
+              </h4>
             </li>
           </ul>
           <h3>&#125; = useForm</h3>
-          {/*
-          const submitUpdatedProfile = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const form = event.currentTarget
-    const formData = new FormData(event.currentTarget)
-    const formDataObject = Object.fromEntries(formData)
-    const response = await fetch('', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application.json' },
-      body: JSON.stringify(formDataObject),
-    })
+          <hr />
 
-    try {
-      if (response) {
-        // WILL BE REMOVED-ONLY TESTING PURPOSE!!
-        alert(
-          `First Name: ${formDataObject.firstName}
-           Last Name: ${formDataObject.lastName}
-            Date Of Birth: ${formDataObject.dateOfBirth}
-            CountrySelected: ${formDataObject.countrySelected}
-            ZipCode: ${formDataObject?.zipCountryCode}
-           Status: SUCCESSFULLY SEND`
-        )
-        reset()
-      }
-    } catch (err) {
-      alert(`${err}`)
-    }
-  }
-          */}
+          <div className="cols">
+            <div className="col-6 has-br">
+              <h3>Next JS Form - useForm hook:</h3>
+            </div>
+            <div className="col-6">
+              <h3>Next JS Form:</h3>
+            </div>
+          </div>
         </div>
       </div>
     </>
