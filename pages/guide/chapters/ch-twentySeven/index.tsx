d65a5e1eeb3fe3d45a36647517ff741cb86d6c3b
michal tomek <95964850+rampepurda@users.noreply.github.com>
2024-02-07 10:18:08 +0100

    Page: Ch27 use client vs server - create

---
+++ pages/guide/chapters/ch-twentySeven/index.tsx
@@ -0,0 +1,56 @@
+import style from '../../../../components/Input/Input.module.scss'
+import { NextPage } from 'next'
+import { Navigation } from '../../../../components'
+import { breakPoints, navigationGuideLinks } from '../../../../configuration'
+import Head from 'next/head'
+import { useWindWidth } from '../../../../hooks'
+import { useEffect } from 'react'
+import Link from 'next/link'
+
+const ChTwentySeven: NextPage = () => {
+  const { windowSize, getWindWidth } = useWindWidth()
+
+  useEffect(() => {
+    getWindWidth()
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>Next JS | Guide | Ch-26 | use client vs use server </title>
+      </Head>
+
+      <div className="cols">
+        {windowSize > breakPoints.isMediumDevice && (
+          <div className="col-3">
+            <Navigation links={navigationGuideLinks} />
+          </div>
+        )}
+
+        <div className="col-9">
+          <h2>27. &apos;use client&apos; vs &apos;use server&apos;</h2>
+          <ul className="hasVerticalPadding-3">
+            <li>
+              <Link href="https://react-hook-form.com/docs/useform" target="_blank">
+                useForm hook - read more
+              </Link>
+            </li>
+            <li>
+              <Link href="https://www.youtube.com/watch?v=R_Pj593TH_Q" target="_blank">
+                See Video Tutorial
+              </Link>
+            </li>
+            <li>
+              <Link href="https://www.youtube.com/watch?v=R_Pj593TH_Q" target="_blank">
+                See Video Tutorial - useForm hook in React
+              </Link>
+            </li>
+          </ul>
+          <hr />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ChTwentySeven
