939d70bdf6667c16d53f27f3915efcd7c626c304
krupicka <tomekmic@centrum.cz>
2024-02-18 15:28:50 +0100

    NG-54| Image_All About

--- pages/guide/chapters/ch-eleven/index.tsx
+++ pages/guide/chapters/ch-eleven/index.tsx
@@ -20,14 +20,45 @@ export const ChEleven: NextPage = () => {
 
         <h3>src</h3>
         <ul className="hasTypeDisc">
-          <li>A statically imported image file, or</li>
+          <li>A statically imported image file</li>
           <li>
             A path string. This can be either an absolute external URL, or an internal path
             depending on the loader prop.
           </li>
         </ul>
-        <h3>External Image</h3>
-        <InfoBox className="info">
+
+        <h3>1. Local Image</h3>
+        <h4>
+          To use a <strong>local image</strong>, <strong>import</strong> your .jpg, .png, or .webp
+          image files. Next.js will <strong>automatically determine the width and height</strong> of
+          your image based on the imported file. These values are used to prevent{' '}
+          <em>Cumulative Layout Shift</em> while your image is loading.
+        </h4>
+        <InfoBox className="isWarning">
+          <h4>
+            Warning: Dynamic await import() or require() are not supported. The import must be
+            static so it can be analyzed at build time.
+          </h4>
+        </InfoBox>
+        <h3>2. Remote Images</h3>
+        <h4>To use a remote image, the src property should be a URL string.</h4>
+        <ul className="hasTypeDisc">
+          <li>
+            Since Next.js does not have access to remote files during the build process, you'll need
+            to provide the <strong>width, height</strong> and optional blurDataURL props manually.
+          </li>
+          <li>
+            The <strong>width and height</strong> attributes are used to infer the correct{' '}
+            <strong>
+              <em>aspect ratio</em>
+            </strong>{' '}
+            of image and avoid layout shift from the image loading in. The width and height do not
+            determine the rendered size of the image file. Learn more about Image Sizing.
+          </li>
+        </ul>
+
+        <h3>3. External Image</h3>
+        <InfoBox className="isWarning">
           <h4>
             External images must be configured in next.config.js using the remotePatterns property.
           </h4>
