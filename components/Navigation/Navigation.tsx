c8dae985b3e43e3f8b58bdb02dfb901968340e95
michal tomek <95964850+rampepurda@users.noreply.github.com>
2024-02-29 13:08:53 +0100

    component: Navigation.tsx clarify styling dependencies

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -17,15 +17,27 @@ type Props = {
 export const Navigation = ({ ClassName, links, isNav }: Props) => {
   const pathName = usePathname()
   const { t } = useTranslation('common')
+  const navigationStyle = () => {
+    switch (isNav) {
+      case `${NavType.Primary}`:
+        return style.primary
+      case `${NavType.Sidebar}`:
+        if (links.length !== 0) {
+          return style.hasBr
+        }
+        break
+      case `${NavType.Accordion}`:
+        return style.accordionNav
+    }
+  }
   const ariaNav = useMemo(() => {
-    if (isNav === NavType.Primary) {
-      return `${t('ariaLabels.navigation.isMain')}`
-    }
-    if (isNav === NavType.Sidebar) {
-      return `${t('ariaLabels.navigation.isLeft')}`
-    }
-    if (isNav === NavType.SubNav) {
-      return `${t('ariaLabels.navigation.isSub')}`
+    switch (isNav) {
+      case `${NavType.Primary}`:
+        return `${t('ariaLabels.navigation.isMain')}`
+      case `${NavType.Sidebar}`:
+        return `${t('ariaLabels.navigation.isLeft')}`
+      case `${NavType.SubNav}`:
+        return `${t('ariaLabels.navigation.isSub')}`
     }
   }, [isNav])
   const ariaLink = useCallback(
@@ -37,15 +49,9 @@ export const Navigation = ({ ClassName, links, isNav }: Props) => {
 
   return (
     <nav
-      className={classNames(
-        ClassName,
-        isNav === NavType.Primary && style.primary,
-        isNav === NavType.Sidebar && links.length !== 0 && style.hasBr,
-        isNav === NavType.Accordion && style.accordionNav,
-        {
-          [`navLeft ${style.sideBar}`]: isNav !== NavType.Primary,
-        }
-      )}
+      className={classNames(ClassName, navigationStyle(), {
+        [`navLeft ${style.sideBar}`]: isNav !== NavType.Primary,
+      })}
       aria-label={ariaNav}
     >
       <ul>
