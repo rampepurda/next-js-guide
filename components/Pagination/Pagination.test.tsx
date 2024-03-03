fd34ecf3a1453d24867a862869eeefabbba40b0f
michal tomek <95964850+rampepurda@users.noreply.github.com>
2024-03-03 12:06:20 +0100

    main | Pagination.module.scss - removed unused commented part of code Pagination.test.tsx - fixed test

--- components/Pagination/Pagination.test.tsx
+++ components/Pagination/Pagination.test.tsx
@@ -1,21 +1,12 @@
 import { render, screen } from '@testing-library/react'
 import { Pagination } from './'
 
-const Expectation: string = 'First and last page in Array have style border-radiusLeft/Right. Expect Links.length'
+const Expectation: string = 'Expect Links correct length'
 
 describe('Pagination', () => {
   test(`${Expectation}`, () => {
-    render(<Pagination
-      currentPage={1}
-      itemsTotal={230}
-      postPerPage={20}
-      paginate={() => {}}
-    />)
-
-    const links = screen.getAllByRole('button')
-
-    expect(links.length).toBe(12)
-    expect(links[0]).toHaveClass("hasRadiusLeft")
-    expect(links[11]).toHaveClass("hasRadiusRight")
+    render(<Pagination currentPage={1} itemsTotal={20} postPerPage={10} paginate={() => {}} />)
+    const btnElement = screen.getAllByRole('button')
+    expect(btnElement.length).toBe(2)
   })
 })
